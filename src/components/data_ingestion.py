76b6f7b42d5bfdf1fe5746e162ab4d6646cc9e7f
faraz1272 <104926607+faraz1272@users.noreply.github.com>
2025-08-10 13:09:31 +0100

    added model training module

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -53,4 +54,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    best_model_name, r2_value = model_trainer.initiate_model_trainer(train_array= train_arr, test_array=test_arr)
+    print(f"Best Model: {best_model_name}, R2 Score: {r2_value}")
